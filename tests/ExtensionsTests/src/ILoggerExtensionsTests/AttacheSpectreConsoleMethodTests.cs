7fcd9c69925287a7901183839cd24b4e713593b4
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-04-14 09:24:00 +0000

    test: add unit test for AttachSpectreConsole method to validate log sink attachment and configuration action

--- tests/ExtensionsTests/src/ILoggerExtensionsTests/AttacheSpectreConsoleMethodTests.cs
+++ tests/ExtensionsTests/src/ILoggerExtensionsTests/AttacheSpectreConsoleMethodTests.cs
@@ -22,4 +22,26 @@ public sealed class TheAttachSpectreConsoleMethod
             .Which.Should()
             .BeOfType<SpectreConsoleLogSink>(because: "the AttachSpectreConsole method should attach a new SpectreConsoleLogSink to the logger");
     }
+
+    [Test]
+    public void ShouldAcceptAnActionThatProvidesAccessToTheSpectreConsoleLogSink()
+    {
+        // Arrange
+        Logger logger = new("Test");
+        SpectreConsoleLogSink? capturedLogSink = null;
+
+        // Act
+        using IDisposable disposable = logger.AttachSpectreConsole(logSink =>
+        {
+            capturedLogSink = logSink;
+        });
+
+        // Assert
+        capturedLogSink.Should().NotBeNull(because: "the configure action should be invoked with the created SpectreConsoleLogSink");
+        logger.AsyncLogSinks.Should()
+            .ContainSingle(because: "the AttachSpectreConsole method should attach exactly one log sink to the logger")
+            .Which.Should()
+            .BeSameAs(capturedLogSink, because: "the SpectreConsoleLogSink passed to the configure action should be the same instance that was attached to the logger");
+        
+    }
 }
