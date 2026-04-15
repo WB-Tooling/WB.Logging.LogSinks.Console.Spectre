9239199f361a985abf6d5b5d008fd3055abd88c1
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-04-15 11:31:03 +0200

    refactor: simplify WriteAsync method and remove null check for ProgressPayload

--- src/LogMessageWriters/ProgressConsoleMessageWriter.cs
+++ src/LogMessageWriters/ProgressConsoleMessageWriter.cs
@@ -12,30 +12,13 @@ internal sealed class ProgressConsoleMessageWriter : IAsyncLogMessageWriter<Prog
 
     public IAsyncLogSink? LogSink { get; set; }
 
-     /// <summary>
-
-    public async ValueTask WriteAsync(DateTimeOffset timestamp, LogLevel? logLevel, IEnumerable<string> senders, ProgressPayload? payload)
+    public async ValueTask WriteAsync(DateTimeOffset timestamp, LogLevel? logLevel, IEnumerable<string> senders, ProgressPayload payload)
     {
-        if (payload is null)
-        {
-            return;
-        }
-
         await Writer.Progress()
             .AutoClear(payload.AutoClear)
             .AutoRefresh(payload.AutoRefresh)
             .HideCompleted(payload.HideCompleted)
-            .StartAsync(async context =>
-            {
-                try
-                {
-                    await payload.Progress(context).ConfigureAwait(false);
-                }
-                catch (Exception ex)
-                {
-                    payload.SetException(ex);
-                }
-            }).ConfigureAwait(false);
+            .StartAsync(payload.Progress).ConfigureAwait(false);
 
         payload.SetCompleted();
     }
