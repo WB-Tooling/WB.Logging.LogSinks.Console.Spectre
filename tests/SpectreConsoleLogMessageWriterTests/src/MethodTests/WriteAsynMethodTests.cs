3ce5878f8ae1d7ef6535c73598166e910a76419b
Wolf Bublitz <wolf.bublitz@gmail.com>
2026-05-02 22:41:32 +0200

    fix: update log message writer assignment to use LogSink.Console

--- tests/SpectreConsoleLogMessageWriterTests/src/MethodTests/WriteAsynMethodTests.cs
+++ tests/SpectreConsoleLogMessageWriterTests/src/MethodTests/WriteAsynMethodTests.cs
@@ -24,7 +24,7 @@ public sealed class TheWriteAsyncMethod
         {
             EmitAnsiSequences = true
         };
-        logMessageWriter.Writer = testConsole;
+        logMessageWriter.LogSink.Console = testConsole;
         logMessageWriter.WriteAsync(
             timestamp: new DateTimeOffset(2024, 6, 1, 12, 0, 0, TimeSpan.Zero),
             logLevel: logLevel,
@@ -48,7 +48,7 @@ public sealed class TheWriteAsyncMethod
         {
             EmitAnsiSequences = true
         };
-        logMessageWriter.Writer = testConsole;
+        logMessageWriter.LogSink.Console = testConsole;
         logMessageWriter.WriteAsync(
             timestamp: new DateTimeOffset(2024, 6, 1, 12, 0, 0, TimeSpan.Zero),
             logLevel: null,
